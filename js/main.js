bd135010dfd0d499e401819494a2f5948677554c
SnsSoll <skek298>
2023-08-11 17:58:20 +0900

    글쓰기 페이지 완료 스프링 mysql연결중

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
   $btnCreate = $("#btn-create");
 
   $btnCreate.click(function () {
-    window.location.assign("../createBoard.html");
+    window.location.href = "../html/createBoard.html";
   });
 });
